9d6f89bae3a747f770893520d2b9e8c8e7bc562b
Josh Lapham <josh@joshlapham.com>
2011-12-27 18:15:40 +1100

    Updated prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,4 +2,6 @@ export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
 autoload -U colors && colors
-PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+#PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+PS1="%{$fg[red]%}%m%{$reset_color%}:%{$fg[green]%}%c%{$reset_color%} %(!.#.$) "
+
