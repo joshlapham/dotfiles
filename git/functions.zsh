1e4bbd175efd1bdd1ff4a204c9a2b565b64d4def
Josh Lapham <josh@joshlapham.com>
2012-03-02 01:56:04 +1100

    Added git functions.zsh

---
+++ git/functions.zsh
@@ -0,0 +1,6 @@
+# add new bare repo to Dropbox and add as remote origin to push to.
+function git_nr() {
+    mkdir ~/Dropbox/repos/$1
+    git init --bare ~/Dropbox/repos/$1
+    git remote add origin ~/Dropbox/repos/$1
+}
