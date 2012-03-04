8985f1f60d0a626c2fed1fbde4d8e8cc1703e48c
Josh Lapham <josh@joshlapham.com>
2012-03-05 01:58:08 +1100

    Added bash bootstrap script as alternative to rakefile.

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script to setup symlinks as alternative to rakefile.
+
+ln -s ~/.dotfiles/zsh/zshrc.symlink ~/.zshrc
+ln -s ~/.dotfiles/screen/screenrc.symlink ~/.screenrc
+ln -s ~/.dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf
+ln -s ~/.dotfiles/vim/vimrc.symlink ~/.vimrc
+ln -s ~/.dotfiles/vim/vim.symlink ~/.vim
