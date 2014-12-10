41ea2fbba2b84e4dade07e564fb8ad8068337ec6
Josh Lapham <josh@joshlapham.com>
2014-12-10 13:16:55 +1000

    Updated OSX set-defaults.sh script

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Idea and some settings 'borrowed' from these guys -
 # https://github.com/mathiasbynens/dotfiles/blob/master/.osx
 # https://github.com/holman/dotfiles/blob/master/osx/set-defaults.sh
@@ -20,9 +22,6 @@ defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Show indicator lights for open applications in the Dock
-defaults write com.apple.dock show-process-indicators -bool true
-
 # Disable disk image verification
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
@@ -33,12 +32,6 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-# Disable the Ping sidebar in iTunes
-defaults write com.apple.iTunes disablePingSidebar -bool true
-
-# Disable all the other Ping stuff in iTunes
-defaults write com.apple.iTunes disablePing -bool true
 
 # Disable Resume system-wide
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
