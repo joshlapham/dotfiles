a0ee607809b7c7173f012adb24952d7d119c1c3f
Josh Lapham <josh@joshlapham.com>
2014-12-10 13:02:53 +1000

    Updated aliases for backup

--- bak/aliases.zsh
+++ bak/aliases.zsh
@@ -1,2 +1,2 @@
-# Backup home folder
-alias bak="rsync -a --exclude .VirtualBox --exclude VirtualBox\ VMs --delete-after /home/jl/ /mnt/bak/newbak/"
+# Backup work folders
+alias bakwork='rsync -avPh --delete {"/Users/joshlapham/Downloads","/Users/joshlapham/Projects"} "/Volumes/™ bak/work"'
