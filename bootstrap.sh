be66160b5bc5b3974b220ba3010bc40f2449635d
Josh Lapham <josh@joshlapham.com>
2014-12-11 08:48:29 +1000

    Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Script to setup symlinks as alternative to rakefile.
+# Script to setup symlinks.
 
 ln -s ~/.dotfiles/zsh/zshrc.symlink ~/.zshrc
-ln -s ~/.dotfiles/screen/screenrc.symlink ~/.screenrc
 ln -s ~/.dotfiles/tmux/tmux.conf.symlink ~/.tmux.conf
 ln -s ~/.dotfiles/vim/vimrc.symlink ~/.vimrc
 ln -s ~/.dotfiles/vim/vim.symlink ~/.vim
+
+echo "Remember to symlink .gitconfig, .gnupg, .mutt, .ssh"
