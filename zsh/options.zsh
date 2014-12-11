248b782c1291ad18ca4e1a7a31b2974833f8a9f0
Josh Lapham <josh@joshlapham.com>
2014-12-11 13:47:58 +1000

    Updated options.zsh

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -13,13 +13,12 @@ setopt menu_complete
 # 10 second wait if you do something that will delete everything.
 setopt RM_STAR_WAIT
 
-# Globbing options
-#setopt NUMERIC_GLOB_SORT
-#setopt EXTENDED_GLOB
-# Case insensitive globbing
-#setopt NO_CASE_GLOB
-# If we have a glob this will expand it
-#setopt GLOB_COMPLETE
+# Enable vi mode
+setopt vi
+
+# Kill the lag when switching vi modes
+export KEYTIMEOUT=1
 
 # Misc
-#setopt RC_EXPAND_PARAM
+# Disable any defaults set by zsh
+unsetopt global_rcs
