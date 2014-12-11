59a29a3fbd626026e4b0f86c208fce3cc2fd21b9
Josh Lapham <josh@joshlapham.com>
2014-12-11 13:20:04 +1000

    Removed functions dir from zsh

--- zsh/functions/wiki.zsh
+++
@@ -1,5 +0,0 @@
-# Grab a short excerpt on a specified topic from Wikipedia
-# USAGE: wiki [whatyouwhattofindwithouthebrackets]
-wiki() {
-    dig +short txt $(echo $* | sed 's/ /_/g').wp.dg.cx|sed -E "s/\" \"|^\"|\"$|\\\\//g"|fmt;
-}
