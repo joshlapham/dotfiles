3bad071cef93968c39671e1403ee43d7ed4cae30
Josh Lapham <josh@joshlapham.com>
2015-01-11 07:22:25 +1000

    Updated git aliases.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,3 +2,7 @@ alias gst='git status'
 alias gb='git branch'
 alias gl='git pull'
 alias gp='git push'
+
+# assume-unchanged
+# List files that are marked as assume-unchanged
+alias ls-unchanged='git ls-files -v | grep "^[[:lower:]]"'
