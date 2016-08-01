890de9fc9ba42589262eedba291a1b603bef1b39
Josh Lapham <josh@joshlapham.com>
2016-08-01 13:11:28 +1000

    Add exports for Android development.

---
+++ android-dev/exports.zsh
@@ -0,0 +1,8 @@
+# NOTE - paths assume SDK was installed with Homebrew.
+# https://gist.github.com/patrickhammond/4ddbe49a67e5eb1b9c03
+export ANT_HOME=/usr/local/opt/ant
+export MAVEN_HOME=/usr/local/opt/maven
+export GRADLE_HOME=/usr/local/opt/gradle
+export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
+
