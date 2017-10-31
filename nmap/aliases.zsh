7fd80b826b0e37841dcf6eb0c7c9dc4291c2f30f
Josh Lapham <josh@joshlapham.com>
2017-11-01 01:04:48 +1000

    Update nmap aliases.

--- nmap/aliases.zsh
+++ nmap/aliases.zsh
@@ -1,2 +1,5 @@
-# Scan local network
+# Quick ping of local network and display hosts that are up
 alias scanlocal='nmap -sP 192.168.1.1/24'
+
+# More detailed scan of local network
+alias scandetailed='sudo nmap -sS -O -F "192.168.1.*"'
