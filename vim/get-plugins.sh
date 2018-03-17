221a4146173bf171f3c512924d85271fc50b108a
Josh Lapham <josh@joshlapham.com>
2018-03-18 09:07:07 +1000

    Disable most vim plugins in get-plugins.sh

--- vim/get-plugins.sh
+++ vim/get-plugins.sh
@@ -24,14 +24,14 @@
 #
 ###
 
-git clone git://github.com/vim-scripts/Command-T.git vim.symlink/bundle/Command-T
-git clone git://github.com/tpope/vim-fugitive.git vim.symlink/bundle/fugitive
+#git clone git://github.com/vim-scripts/Command-T.git vim.symlink/bundle/Command-T
+#git clone git://github.com/tpope/vim-fugitive.git vim.symlink/bundle/fugitive
 git clone git://github.com/vim-scripts/Gundo.git vim.symlink/bundle/Gundo
-git clone git://github.com/scrooloose/nerdtree.git vim.symlink/bundle/nerdtree
-git clone git://github.com/msanders/snipmate.vim.git vim.symlink/bundle/snipMate
+#git clone git://github.com/scrooloose/nerdtree.git vim.symlink/bundle/nerdtree
+#git clone git://github.com/msanders/snipmate.vim.git vim.symlink/bundle/snipMate
 #git clone git://github.com/ervandew/supertab.git vim.symlink/bundle/supertab
 #git clone git://github.com/vim-scripts/taglist.vim.git vim.symlink/bundle/taglist
-git clone git://github.com/vim-scripts/VOoM.git vim.symlink/bundle/voom
+#git clone git://github.com/vim-scripts/VOoM.git vim.symlink/bundle/voom
 git clone git://github.com/altercation/vim-colors-solarized.git vim.symlink/bundle/vim-colors-solarized
 #git clone git://github.com/kevinw/pyflakes-vim.git vim.symlink/bundle/pyflakes
 #git clone git://github.com/vim-scripts/Pydiction.git vim.symlink/bundle/Pydiction
