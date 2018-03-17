89de3169287e532bdc8252d77e4aa25e756fce60
Josh Lapham <josh@joshlapham.com>
2018-03-18 09:05:47 +1000

    Update prompt.zsh to remove hostname from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,5 +2,8 @@
 #PS1="%{$fg[red]%}%m%{$reset_color%}:%{$fg[green]%}%c%{$reset_color%} %(!.#.$) "
 
 # Other prompt examples
-PS1="%{$fg[red]%}%m%{$reset_color%}:%{$fg[green]%}%~%{$reset_color%} %(!.#.$) "
+# With hostname
+#PS1="%{$fg[red]%}%m%{$reset_color%}:%{$fg[green]%}%~%{$reset_color%} %(!.#.$) "
+# Without hostname
+PS1="%{$fg[red]%}%{$reset_color%}%{$fg[green]%}%~%{$reset_color%} %(!.#.$) "
 #PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
