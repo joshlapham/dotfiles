2ca4dfdc974683bb5bd40462de397b4519063143
Josh Lapham <josh@joshlapham.com>
2019-05-08 19:32:11 +1000

    Fix GREP_OPTIONS error

--- zsh/grep.zsh
+++ zsh/grep.zsh
@@ -1,6 +1,2 @@
-#
-# Color grep results
-# Examples: http://rubyurl.com/ZXv
-#
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'%
+alias grep='grep --color=auto'
+export GREP_COLOR='1;32'
