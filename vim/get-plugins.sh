f38078f7e97ab3416f4adee3597d001aad56a1fd
Josh Lapham <josh@joshlapham.com>
2020-02-10 15:36:56 +1000

    Update paths in vim plugins script

--- vim/get-plugins.sh
+++ vim/get-plugins.sh
@@ -24,17 +24,17 @@
 #
 ###
 
-#git clone git://github.com/vim-scripts/Command-T.git vim.symlink/bundle/Command-T
-#git clone git://github.com/tpope/vim-fugitive.git vim.symlink/bundle/fugitive
-git clone git://github.com/vim-scripts/Gundo.git vim.symlink/bundle/Gundo
-#git clone git://github.com/scrooloose/nerdtree.git vim.symlink/bundle/nerdtree
-#git clone git://github.com/msanders/snipmate.vim.git vim.symlink/bundle/snipMate
-#git clone git://github.com/ervandew/supertab.git vim.symlink/bundle/supertab
-#git clone git://github.com/vim-scripts/taglist.vim.git vim.symlink/bundle/taglist
-#git clone git://github.com/vim-scripts/VOoM.git vim.symlink/bundle/voom
-git clone git://github.com/altercation/vim-colors-solarized.git vim.symlink/bundle/vim-colors-solarized
-#git clone git://github.com/kevinw/pyflakes-vim.git vim.symlink/bundle/pyflakes
-#git clone git://github.com/vim-scripts/Pydiction.git vim.symlink/bundle/Pydiction
-git clone git://github.com/scrooloose/syntastic.git vim.symlink/bundle/syntastic
+#git clone git://github.com/vim-scripts/Command-T.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/Command-T
+#git clone git://github.com/tpope/vim-fugitive.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/fugitive
+git clone git://github.com/vim-scripts/Gundo.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/Gundo
+#git clone git://github.com/scrooloose/nerdtree.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/nerdtree
+#git clone git://github.com/msanders/snipmate.vim.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/snipMate
+#git clone git://github.com/ervandew/supertab.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/supertab
+#git clone git://github.com/vim-scripts/taglist.vim.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/taglist
+#git clone git://github.com/vim-scripts/VOoM.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/voom
+git clone git://github.com/altercation/vim-colors-solarized.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/vim-colors-solarized
+#git clone git://github.com/kevinw/pyflakes-vim.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/pyflakes
+#git clone git://github.com/vim-scripts/Pydiction.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/Pydiction
+git clone git://github.com/scrooloose/syntastic.git /home/$USER/.dotfiles/vim/vim.symlink/bundle/syntastic
 
 echo "Plugin clone complete."
